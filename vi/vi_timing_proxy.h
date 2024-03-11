d1da88e1ad3d2aa26af451ca5f78aed5519676cf
Ilya Tikhomirov <Uff1965@rambler.ru>
2024-03-11 16:00:36 +0300

    xxx

--- vi/vi_timing_proxy.h
+++ vi/vi_timing_proxy.h
@@ -1,15 +1,18 @@
-#ifndef _CM_TIMING_PROXY_H_
-#	define _CM_TIMING_PROXY_H_ "1.0.0.1"
+#ifndef _VI_TIMING_PROXY_H_
+#	define _VI_TIMING_PROXY_H_ "3.0.0.1"
 #	pragma once
 
-#	if defined __cplusplus && !defined(CM_TM_DISABLE) && __has_include(<cm_timing/cm_timing.h>)
-#		include <cm_timing/cm_timing.h>
+#	if !defined(VI_TM_DISABLE) && __has_include(<vi/timing.h>)
+#		include <vi/timing.h>
 #	else
-#		define CM_TM_DISABLE 1
-#		define CM_TM(...) ((void)0)
-#		define CM_TM_FUNC ((void)0)
-#		define CM_TM_INIT(s, ...)
-#		define CM_TM_REPORT(...) ((void)0)
+#		define VI_STR_AUX( a, b ) a##b
+#		define VI_STR( a, b ) VI_STR_AUX( a, b )
+#		define VI_MAKE_UNIC_ID( prefix ) VI_STR( prefix, __LINE__ )
+
+#		define VI_TM_INIT(...) int VI_MAKE_UNIC_ID(_vi_tm_dummy_) = (__VA_ARGS__, 0)
+#		define VI_TM(...) int VI_MAKE_UNIC_ID(_vi_tm_dummy_) = (__VA_ARGS__, 0)
+#		define VI_TM_REPORT(...) ((void)(__VA_ARGS__, 0))
+#		define VI_TM_FUNC ((void)0)
 #	endif
 
-#endif // #ifndef _CM_TIMING_PROXY_H_
+#endif // #ifndef _VI_TIMING_PROXY_H_
