711d7071943cd7879daa5e60cd5965e3699895ab
Ilya Tikhomirov <Uff1965@rambler.ru>
2024-11-04 21:12:53 +0300

    Add vi_tmWarming and refactoring.

--- vi_timing/common.h
+++ vi_timing/common.h
@@ -33,12 +33,14 @@ If not, see <https://www.gnu.org/licenses/gpl-3.0.html#license-text>.
 #	define VI_NOEXCEPT noexcept
 #	define VI_R_CAST(T, s) reinterpret_cast<T>(s)
 #	define VI_S_CAST(T, s) static_cast<T>(s)
+#	define VI_DEFAULT(v) = (v)
 #else
 #	define VI_STD(s) s
 #	define VI_MEMORY_ORDER(s) s
 #	define VI_NOEXCEPT
 #	define VI_R_CAST(T, s) (T)s
 #	define VI_S_CAST(T, s) (T)s
+#	define VI_DEFAULT(v)
 #endif
 
 #ifdef _MSC_VER
