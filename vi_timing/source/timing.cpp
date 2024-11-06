a12a133e71d960c7d99e696b850bd5a7cc419138
Ilya Tikhomirov <Uff1965@rambler.ru>
2024-11-06 20:21:49 +0300

    Fix Linux build

--- vi_timing/source/timing.cpp
+++ vi_timing/source/timing.cpp
@@ -32,6 +32,7 @@ If not, see <https://www.gnu.org/licenses/gpl-3.0.html#license-text>.
 
 #include <cassert>
 #include <chrono>
+#include <cstring>
 #include <mutex>
 #include <string>
 #include <thread>
