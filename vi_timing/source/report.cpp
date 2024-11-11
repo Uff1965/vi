cc7ddc19705d8260b2fb490feb2ebf8676b6ac9e
Ilya Tikhomirov <Uff1965@rambler.ru>
2024-11-11 20:56:52 +0300

    step

--- vi_timing/source/report.cpp
+++ vi_timing/source/report.cpp
@@ -31,7 +31,6 @@ If not, see <https://www.gnu.org/licenses/gpl-3.0.html#license-text>.
 #include <timing.h>
 
 #include <algorithm>
-//#include <atomic>
 #include <cassert>
 #include <cfloat>
 #include <chrono>
