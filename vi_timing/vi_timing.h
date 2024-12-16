0587b5ae0d0cb6339a042c4c7966e821648601b2
Ilya Tikhomirov <Uff1965@rambler.ru>
2024-12-16 11:55:50 +0300

    Removed vi_timing.h.in

--- vi_timing/vi_timing.h
+++ vi_timing/vi_timing.h
@@ -28,7 +28,7 @@ If not, see <https://www.gnu.org/licenses/gpl-3.0.html#license-text>.
 #	pragma once
 
 #	define VI_TM_VERSION_MAJOR 0	// 0 - 99
-#	define VI_TM_VERSION_MINOR 15	// 0 - 999
+#	define VI_TM_VERSION_MINOR 3	// 0 - 999
 #	define VI_TM_VERSION_PATCH 1	// 0 - 9999
 #	define VI_TM_VERSION (((VI_TM_VERSION_MAJOR) * 1000U + (VI_TM_VERSION_MINOR)) * 10000U + (VI_TM_VERSION_PATCH))
 #	define VI_TM_VERSION_STR VI_STR(VI_TM_VERSION_MAJOR) "." VI_STR(VI_TM_VERSION_MINOR) "." VI_STR(VI_TM_VERSION_PATCH)
