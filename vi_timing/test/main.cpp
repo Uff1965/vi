ef0105ac4aaa95dda85417a33e72c4b44cf58850
Ilya Tikhomirov <Uff1965@rambler.ru>
2024-12-21 18:33:41 +0300

    Step

--- vi_timing/test/main.cpp
+++ vi_timing/test/main.cpp
@@ -2,33 +2,38 @@
 //#define VI_TM_DISABLE
 #include <vi_timing/vi_timing.h>
 
+#include <thread>
+
 namespace
 {	VI_TM_INIT(vi_tmSortBySpeed, vi_tmShowResolution, vi_tmShowDuration);
-	VI_TM("Global");
+	VI_TM("GLOBAL");
 }
 
 int main(int argn, char* args[])
 {	VI_TM_FUNC;
-	constexpr auto CNT = 10'000;
+	constexpr auto CNT = 1'000;
 	printf("Version of \'vi_timing\' library: %s\n\n", VI_TM_FULLVERSION);
 	
-	static const auto cnt = argn > 1 ? std::atoi(args[1]) : 10'000;
-	printf("n = %d\n\n", cnt);
+	static const auto cnt = argn > 1 ? std::atoi(args[1]) : 1'000;
+	printf("Burden: %d\n\n", cnt);
 
 	static volatile auto dummy = 0U;
-	auto foo = []()
-		{	for (auto n = cnt; n--; )
+	auto burden = []()
+		{	for (auto n = cnt; n; --n)
 				++dummy;
 		};
 
-	for (auto n = 0; n < CNT; ++n)
-	{	VI_TM("Separately");
-		foo();
-	}
+	for (int m = 0; m < 1'000; ++m)
+	{
+		for (auto n = 0; n < CNT; ++n)
+		{	VI_TM("ALONE");
+			burden();
+		}
 
-	{	VI_TM("Together", CNT);
-		for (int n = 0; n < CNT; ++n)
-		{	foo();
+		{	VI_TM("JOINTLY", CNT);
+			for (int n = 0; n < CNT; ++n)
+			{	burden();
+			}
 		}
 	}
 }
