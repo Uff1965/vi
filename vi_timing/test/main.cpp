5851d1e3a3c65bf44c9cb43779f01d75c32b6c18
Ilya Tikhomirov <Uff1965@rambler.ru>
2024-12-24 14:35:24 +0300

    step

--- vi_timing/test/main.cpp
+++ vi_timing/test/main.cpp
@@ -9,7 +9,7 @@ namespace
 	VI_TM("GLOBAL");
 
 	void burden(int cnt)
-	{	static std::atomic<unsigned> dummy = 0U;
+	{	static volatile unsigned dummy = 0U;
 		for (auto n = 0; n < cnt; ++n)
 			++dummy;
 	};
@@ -32,94 +32,73 @@ int main(int argn, char* args[])
 	constexpr auto CNT = 1'000;
 	for (int m = 0; m < 100; ++m)
 	{
-		{
-			for (auto n = 0; n < CNT; ++n)
-			{	VI_TM("ALONE volatile");
-				burden(cnt);
-			}
-
-			{	VI_TM("GROUP volatile", CNT);
-				for (int n = 0; n < CNT; ++n)
-				{	burden(cnt);
-				}
+		for (auto n = 0; n < CNT; ++n)
+		{	VI_TM("ALONE volatile");
+			burden(cnt);
+		}
+		{	VI_TM("GROUP volatile", CNT);
+			for (int n = 0; n < CNT; ++n)
+			{	burden(cnt);
 			}
 		}
 
-		{
-			for (auto n = 0; n < CNT; ++n)
-			{	VI_TM("ALONE seq_cst");
-				burden<std::memory_order_seq_cst>(cnt);
-			}
-
-			{	VI_TM("GROUP seq_cst", CNT);
-				for (int n = 0; n < CNT; ++n)
-				{	burden<std::memory_order_seq_cst>(cnt);
-				}
+		for (auto n = 0; n < CNT; ++n)
+		{	VI_TM("ALONE seq_cst");
+			burden<std::memory_order_seq_cst>(cnt);
+		}
+		{	VI_TM("GROUP seq_cst", CNT);
+			for (int n = 0; n < CNT; ++n)
+			{	burden<std::memory_order_seq_cst>(cnt);
 			}
 		}
 
-		{
-			for (auto n = 0; n < CNT; ++n)
-			{	VI_TM("ALONE relaxed");
-				burden<std::memory_order_relaxed>(cnt);
-			}
-
-			{	VI_TM("GROUP relaxed", CNT);
-				for (int n = 0; n < CNT; ++n)
-				{	burden<std::memory_order_relaxed>(cnt);
-				}
+		for (auto n = 0; n < CNT; ++n)
+		{	VI_TM("ALONE relaxed");
+			burden<std::memory_order_relaxed>(cnt);
+		}
+		{	VI_TM("GROUP relaxed", CNT);
+			for (int n = 0; n < CNT; ++n)
+			{	burden<std::memory_order_relaxed>(cnt);
 			}
 		}
 
-		{
-			for (auto n = 0; n < CNT; ++n)
-			{	VI_TM("ALONE release");
-				burden<std::memory_order_release>(cnt);
-			}
-
-			{	VI_TM("GROUP release", CNT);
-				for (int n = 0; n < CNT; ++n)
-				{	burden<std::memory_order_release>(cnt);
-				}
+		for (auto n = 0; n < CNT; ++n)
+		{	VI_TM("ALONE release");
+			burden<std::memory_order_release>(cnt);
+		}
+		{	VI_TM("GROUP release", CNT);
+			for (int n = 0; n < CNT; ++n)
+			{	burden<std::memory_order_release>(cnt);
 			}
 		}
 
-		{
-			for (auto n = 0; n < CNT; ++n)
-			{	VI_TM("ALONE acquire");
-				burden<std::memory_order_acquire>(cnt);
-			}
-
-			{	VI_TM("GROUP acquire", CNT);
-				for (int n = 0; n < CNT; ++n)
-				{	burden<std::memory_order_acquire>(cnt);
-				}
+		for (auto n = 0; n < CNT; ++n)
+		{	VI_TM("ALONE acquire");
+			burden<std::memory_order_acquire>(cnt);
+		}
+		{	VI_TM("GROUP acquire", CNT);
+			for (int n = 0; n < CNT; ++n)
+			{	burden<std::memory_order_acquire>(cnt);
 			}
 		}
 
-		{
-			for (auto n = 0; n < CNT; ++n)
-			{	VI_TM("ALONE acq_rel");
-				burden<std::memory_order_acq_rel>(cnt);
-			}
-
-			{	VI_TM("GROUP acq_rel", CNT);
-				for (int n = 0; n < CNT; ++n)
-				{	burden<std::memory_order_acq_rel>(cnt);
-				}
+		for (auto n = 0; n < CNT; ++n)
+		{	VI_TM("ALONE acq_rel");
+			burden<std::memory_order_acq_rel>(cnt);
+		}
+		{	VI_TM("GROUP acq_rel", CNT);
+			for (int n = 0; n < CNT; ++n)
+			{	burden<std::memory_order_acq_rel>(cnt);
 			}
 		}
 
-		{
-			for (auto n = 0; n < CNT; ++n)
-			{	VI_TM("ALONE consume");
-				burden<std::memory_order_consume>(cnt);
-			}
-
-			{	VI_TM("GROUP consume", CNT);
-				for (int n = 0; n < CNT; ++n)
-				{	burden<std::memory_order_consume>(cnt);
-				}
+		for (auto n = 0; n < CNT; ++n)
+		{	VI_TM("ALONE consume");
+			burden<std::memory_order_consume>(cnt);
+		}
+		{	VI_TM("GROUP consume", CNT);
+			for (int n = 0; n < CNT; ++n)
+			{	burden<std::memory_order_consume>(cnt);
 			}
 		}
 	}
