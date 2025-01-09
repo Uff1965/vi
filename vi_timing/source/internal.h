90fbc208c70866d5a49895780b7768eee67950f5
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-01-09 13:30:28 +0300

    Refactoring

--- vi_timing/source/internal.h
+++ vi_timing/source/internal.h
@@ -10,7 +10,7 @@
 
 namespace misc
 {
-	unsigned build_number_updater(const char (&date)[12], const char (&time)[9]); // Converts the date and time to a monotonically increasing number. Stores it in BUILD_NUMBER.
+	constexpr unsigned build_number_updater(const char (&date)[12], const char (&time)[9]); // Converts the date and time to a monotonically increasing number. Stores it in BUILD_NUMBER.
 	static const auto dummy_build_number_updater = build_number_updater(__DATE__, __TIME__);
 
 	struct space_out final: std::numpunct<char>
--- vi_timing/source/misc.cpp
+++ vi_timing/source/misc.cpp
@@ -180,7 +180,7 @@ namespace
 
 } // namespace
 
-unsigned misc::build_number_updater(const char (&date)[12], const char (&time)[9])
+constexpr unsigned misc::build_number_updater(const char (&date)[12], const char (&time)[9])
 {	build_number = std::max(TIME_STAMP(date, time), build_number);
 	return build_number;
 }
