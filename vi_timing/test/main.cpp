bec39934e5819fca6591c32f20d4b90c3deed399
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-01-13 13:07:48 +0300

    Refactroing

--- vi_timing/test/main.cpp
+++ vi_timing/test/main.cpp
@@ -2,6 +2,8 @@
 //#define VI_TM_DISABLE
 #include <vi_timing/vi_timing.h>
 
+#include <thread>
+
 namespace
 {	VI_TM_INIT(vi_tmSortBySpeed, vi_tmShowResolution, vi_tmShowDuration);
 	VI_TM("GLOBAL");
@@ -10,11 +12,11 @@ namespace
 int main(int argn, char* args[])
 {	VI_TM_FUNC;
 	printf("Version of \'vi_timing\' library: %s\n\n", VI_TM_FULLVERSION);
-	
+
 	const auto cnt1 = argn == 2 ? std::atoi(args[1]) : 50;
 	const auto cnt10 = 10 * cnt1;
 	printf("Burden: %d\n\n", cnt1);
-
+	
 	auto burden = [](int cnt)
 		{	static volatile auto dummy = 0U;
 			for (auto n = cnt; n; --n)
@@ -29,22 +31,20 @@ int main(int argn, char* args[])
 			burden(cnt1);
 		}
 
-		for (auto n = 0; n < CNT; ++n)
-		{	VI_TM("ALONE 10");
-			burden(cnt10);
-		}
-
 		{	VI_TM("GROUP", CNT);
 			for (int n = 0; n < CNT; ++n)
 			{	burden(cnt1);
 			}
 		}
 
-		{	VI_TM("GROUP 10", CNT);
-			for (int n = 0; n < CNT; ++n)
-			{	burden(cnt10);
-			}
+		for (auto n = 0; n < CNT; ++n)
+		{	VI_TM("ALONE 10");
+			burden(cnt10);
 		}
+	}
+
+	{	VI_TM("ALONE SINGLE");
+		burden(cnt1);
 	}
 
 	const double *overhead = reinterpret_cast<const double *>(vi_tmInfo(VI_TM_INFO_OVERHEAD));
@@ -52,7 +52,7 @@ int main(int argn, char* args[])
 	printf("Overhead: %.3g ticks;\nUnit: %.3g sec.\n\n", *overhead, *unit);
 
 	auto fn = [](const char *name, VI_TM_TICK total, std::size_t amount, std::size_t calls_cnt, void *)
-		{	printf("%-10s: total = %10zd, amount = %6zd, calls = %6zd\n", name, total, amount, calls_cnt);
+		{	printf("%-16s: total = %10zd, amount = %6zd, calls = %6zd\n", name, total, amount, calls_cnt);
 			return 0;
 		};
 	vi_tmResults(nullptr, fn, nullptr);
