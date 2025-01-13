4f3407ec3810773125e4f88eb4e17d0409615517
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-01-13 10:31:48 +0300

    Refactoring

--- vi_timing/test/main.cpp
+++ vi_timing/test/main.cpp
@@ -11,27 +11,50 @@ int main(int argn, char* args[])
 {	VI_TM_FUNC;
 	printf("Version of \'vi_timing\' library: %s\n\n", VI_TM_FULLVERSION);
 	
-	static const auto cnt = argn == 2 ? std::atoi(args[1]) : 1'000;
-	printf("Burden: %d\n\n", cnt);
+	const auto cnt1 = argn == 2 ? std::atoi(args[1]) : 50;
+	const auto cnt10 = 10 * cnt1;
+	printf("Burden: %d\n\n", cnt1);
 
-	static volatile auto dummy = 0U;
-	auto burden = []()
-		{	for (auto n = cnt; n; --n)
+	auto burden = [](int cnt)
+		{	static volatile auto dummy = 0U;
+			for (auto n = cnt; n; --n)
 				++dummy;
 		};
 
-	constexpr auto CNT = 1'000;
 	for (int m = 0; m < 100; ++m)
-	{
+	{	constexpr auto CNT = 1'000;
+
 		for (auto n = 0; n < CNT; ++n)
 		{	VI_TM("ALONE");
-			burden();
+			burden(cnt1);
+		}
+
+		for (auto n = 0; n < CNT; ++n)
+		{	VI_TM("ALONE 10");
+			burden(cnt10);
 		}
 
 		{	VI_TM("GROUP", CNT);
 			for (int n = 0; n < CNT; ++n)
-			{	burden();
+			{	burden(cnt1);
+			}
+		}
+
+		{	VI_TM("GROUP 10", CNT);
+			for (int n = 0; n < CNT; ++n)
+			{	burden(cnt10);
 			}
 		}
 	}
+
+	const double *overhead = reinterpret_cast<const double *>(vi_tmInfo(VI_TM_INFO_OVERHEAD));
+	const double *unit = reinterpret_cast<const double *>(vi_tmInfo(VI_TM_INFO_UNIT));
+	printf("Overhead: %.4g\nUnit: %.4g sec.\n", *overhead, *unit);
+
+	auto fn = [](const char *name, VI_TM_TICK total, std::size_t amount, std::size_t calls_cnt, void *)
+		{	printf("%10s: total = %10zd, amount = %6zd, calls = %6zd\n", name, total, amount, calls_cnt);
+			return 0;
+		};
+	vi_tmResults(nullptr, fn, nullptr);
+	puts("");
 }
