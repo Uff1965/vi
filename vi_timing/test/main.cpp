4e6dd63639b5d915e8e1c2c613fc9c570244230c
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-01-19 23:51:46 +0300

    atomic added

--- vi_timing/test/main.cpp
+++ vi_timing/test/main.cpp
@@ -1,5 +1,6 @@
 //#define VI_TM_DISABLE // Uncomment this line to remove timing.
 #include <vi_timing/vi_timing.h>
+#include <atomic>
 
 namespace
 {
@@ -11,13 +12,14 @@ namespace
 	constexpr char NOTHING[] = "Nothing";
 }
 
-//VI_OPTIMIZE_OFF
 int main()
 {	VI_TM_FUNC;
 
-	vi_tmThreadPrepare();
-	VI_TM_CLEAR(GROUP);
-	VI_TM_CLEAR(NOTHING);
+	{	// Optional.
+		vi_tmThreadPrepare(); // Fix the CPU affinity for the current thread, warms up the CPU and waits for the next time quantum to start.
+		VI_TM_CLEAR(GROUP);
+		VI_TM_CLEAR(NOTHING);
+	}
 
 	{	VI_TM(GROUP, CNT);
 		for (int n = 0; n < CNT; ++n)
@@ -25,8 +27,43 @@ int main()
 		}
 	}
 
+	constexpr auto N = 10'000U;
+	{	volatile int i = 0;
+		VI_TM("volatile", N);
+		for (unsigned n = 0; n < N; ++n)
+		{	i = 777;
+		}
+	}
+
+	{	std::atomic<int> i = 0;
+		VI_TM("atomic", N);
+		for (unsigned n = 0; n < N; ++n)
+		{	i = 777;
+		}
+	}
+
+	{	std::atomic<int> i = 0;
+		VI_TM("seq_cst", N);
+		for (unsigned n = 0; n < N; ++n)
+		{	i.store(777, std::memory_order_seq_cst);
+		}
+	}
+
+	{	std::atomic<int> i = 0;
+		VI_TM("relaxed", N);
+		for (unsigned n = 0; n < N; ++n)
+		{	i.store(777, std::memory_order_relaxed);
+		}
+	}
+
+	{	std::atomic<int> i = 0;
+		VI_TM("release", N);
+		for (unsigned n = 0; n < N; ++n)
+		{	i.store(777, std::memory_order_release);
+		}
+	}
+
 	vi_tmThreadAffinityRestore();
 
 	puts("Hello, World!\n");
 }
-//VI_OPTIMIZE_ON
