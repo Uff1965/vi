7a0ec91935fcf31f0ac68d08f8c379b965e84895
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-04-08 12:28:41 +0300

    step

--- vi_timing/source/misc.cpp
+++ vi_timing/source/misc.cpp
@@ -163,15 +163,6 @@ namespace
 
 	unsigned build_number = 0U; // build_number is initialized by the compilation time of the last unit of translation in the YYMMDDhhmm format.
 
-	[[nodiscard]] double round_ext(double num, unsigned char significant)
-	{	assert(significant > 0);
-		if (0 == significant || std::isnan(num) || std::isinf(num) || 0.0 == num)
-			return num;
-
-		const auto exp = std::ceil(std::log10(std::abs(num)));
-		const auto factor = std::pow(10.0, significant - exp);
-		return std::round(num * factor) / factor;
-	}
 } // namespace
 
 /// <summary>
@@ -194,28 +185,31 @@ namespace
 	{	result = "NaN";
 	}
 	else if (std::isinf(num))
-	{	result = (num > .0) ? "INF" : "-INF";
+	{	result = std::signbit(num) ? "-INF" : "INF";
 	}
 	else
-	{	num = round_ext(num, significant);
-
+	{
 		struct
 		{	std::string_view suffix_;
 			double factor_;
 		} unit = { " ps", 1e12 };
 
-		if (std::abs(num) < 1e-12)
+		if (std::isless(std::abs(num), 1e-12))
 		{	num = 0.0;
 		}
 		else
-		{	constexpr auto GROUP = 3;
-			const auto supp = ((significant - decimal - 1) / GROUP) * GROUP - log10(std::abs(num));
-			if (supp <= -6) { unit = { " Ms", 1e-6 }; }
-			else if (supp <= -3) { unit = { " ks", 1e-3 }; }
-			else if (supp <= 0) { unit = { " s ", 1e+0 }; }
-			else if (supp <= 3) { unit = { " ms", 1e+3 }; }
-			else if (supp <= 6) { unit = { " us", 1e+6 }; }
-			else if (supp <= 9) { unit = { " ns", 1e+9 }; }
+		{	const auto exp = std::log10(std::abs(num));
+			const auto factor = std::pow(10.0, significant - std::ceil(exp));
+			num = std::round(num * factor) / factor;
+
+			constexpr auto GROUP = 3;
+			const auto supp = ((significant - decimal - 1) / GROUP) * GROUP - exp;
+			if (std::islessequal(supp, -6)) { unit = { " Ms", 1e-6 }; }
+			else if (std::islessequal(supp, -3)) { unit = { " ks", 1e-3 }; }
+			else if (std::islessequal(supp, 0)) { unit = { " s ", 1e+0 }; }
+			else if (std::islessequal(supp, 3)) { unit = { " ms", 1e+3 }; }
+			else if (std::islessequal(supp, 6)) { unit = { " us", 1e+6 }; }
+			else if (std::islessequal(supp, 9)) { unit = { " ns", 1e+9 }; }
 		}
 
 		std::ostringstream ss;
@@ -225,8 +219,6 @@ namespace
 
 	return result;
 }
-
-
 
 unsigned misc::build_number_updater(const char (&date)[12], const char (&time)[9])
 {	build_number = std::max(time_stamp(date, time), build_number);
@@ -352,111 +344,87 @@ std::uintptr_t VI_TM_CALL vi_tmInfo(vi_tmInfo_e info)
 namespace
 {
 	const auto nanotests = []
-	{
-		{	// nanotest for round_ext
-			assert(0.0 == round_ext(0.00, 1));
+	{	// nanotest for misc::to_string(std::chrono::duration<double> d, unsigned char precision, unsigned char dec)
+		const struct
+		{
+			int line_;
+			double num_;
+			std::string_view expected_;
+			unsigned char significant_;
+			unsigned char decimal_;
+		} tests_set[] =
+		{
+			{ __LINE__, .0, "0 ps", 1, 0 },
+			{ __LINE__, .0, "0.00 ps", 7, 2 },
+			{ __LINE__, -9e-13, "0.00 ps", 7, 2 },
+			{ __LINE__, 9e-13, "0 ps", 1, 0 },
+			{ __LINE__, -1e-12, "-1 ps", 1, 0 },
+			{ __LINE__, 1e-12, "1 ps", 1, 0 },
+			{ __LINE__, 1e-12, "1.00 ps", 7, 2 },
+			{ __LINE__, -1e9, "-1000 Ms", 1, 0 },
+			{ __LINE__, 1e9, "1000 Ms", 1, 0 },
+			{ __LINE__, 1e9, "1000.00 Ms", 7, 2 },
 
-			assert(1.0 == round_ext(0.95, 1));
-			assert(1.0 == round_ext(1.00, 1));
-			assert(1.0 == round_ext(1.40, 1));
-			assert(-1.0 == round_ext(-0.95, 1));
-			assert(-1.0 == round_ext(-1.00, 1));
-			assert(-1.0 == round_ext(-1.40, 1));
+			{ __LINE__, .00555555555,        "6 ms", 1, 0 },
+			{ __LINE__, .05555555555,       "60 ms", 1, 0 },
+			{ __LINE__, .55555555555,      "600 ms", 1, 0 },
+			{ __LINE__, 5.5555555555,        "6 s ", 1, 0 },
+			{ __LINE__, 55.555555555,       "60 s ", 1, 0 },
+			{ __LINE__, 555.55555555,      "600 s ", 1, 0 },
+			{ __LINE__, 5555.5555555,        "6 ks", 1, 0 },
 
-			assert(0.10 == round_ext(.0995, 2));
-			assert(0.10 == round_ext(.1000, 2));
-			assert(0.10 == round_ext(.1044, 2));
-			assert(-0.10 == round_ext(-.0995, 2));
-			assert(-0.10 == round_ext(-.1000, 2));
-			assert(-0.10 == round_ext(-.1044, 2));
+			{ __LINE__, .00555555555,        "6 ms", 3, 0 },
+			{ __LINE__, .05555555555,       "56 ms", 3, 0 },
+			{ __LINE__, .55555555555,      "556 ms", 3, 0 },
+			{ __LINE__, 5.5555555555,        "6 s ", 3, 0 },
+			{ __LINE__, 55.555555555,       "56 s ", 3, 0 },
+			{ __LINE__, 555.55555555,      "556 s ", 3, 0 },
+			{ __LINE__, 5555.5555555,        "6 ks", 3, 0 },
 
-			assert(1.0e-16 == round_ext(0.95e-16, 1));
-			assert(1.0e-16 == round_ext(1.00e-16, 1));
-			assert(1.0e-16 == round_ext(1.40e-16, 1));
-		}
+			{ __LINE__, .00555555555,     "5556 us", 4, 0 },
+			{ __LINE__, .05555555555,    "55560 us", 4, 0 },
+			{ __LINE__, .55555555555,   "555600 us", 4, 0 },
+			{ __LINE__, 5.5555555555,     "5556 ms", 4, 0 },
+			{ __LINE__, 55.555555555,    "55560 ms", 4, 0 },
+			{ __LINE__, 555.55555555,   "555600 ms", 4, 0 },
+			{ __LINE__, 5555.5555555,     "5556 s ", 4, 0 },
 
-		{	// nanotest for misc::to_string(std::chrono::duration<double> d, unsigned char precision, unsigned char dec)
-			const struct
-			{
-				int line_;
-				double num_;
-				std::string_view expected_;
-				unsigned char significant_;
-				unsigned char decimal_;
-			} tests_set[] =
-			{
-				{ __LINE__, .0, "0 ps", 1, 0 },
-				{ __LINE__, .0, "0.00 ps", 7, 2 },
-				{ __LINE__, -9e-13, "0.00 ps", 7, 2 },
-				{ __LINE__, 9e-13, "0 ps", 1, 0 },
-				{ __LINE__, -1e-12, "-1 ps", 1, 0 },
-				{ __LINE__, 1e-12, "1 ps", 1, 0 },
-				{ __LINE__, 1e-12, "1.00 ps", 7, 2 },
-				{ __LINE__, -1e9, "-1000 Ms", 1, 0 },
-				{ __LINE__, 1e9, "1000 Ms", 1, 0 },
-				{ __LINE__, 1e9, "1000.00 Ms", 7, 2 },
+			{ __LINE__, .00555555555,      "5.6 ms", 2, 1 },
+			{ __LINE__, .05555555555,     "56.0 ms", 2, 1 },
+			{ __LINE__, .55555555555,    "560.0 ms", 2, 1 },
+			{ __LINE__, 5.5555555555,      "5.6 s ", 2, 1 },
+			{ __LINE__, 55.555555555,     "56.0 s ", 2, 1 },
+			{ __LINE__, 555.55555555,    "560.0 s ", 2, 1 },
+			{ __LINE__, 5555.5555555,      "5.6 ks", 2, 1 },
 
-				{ __LINE__, .00555555555,        "6 ms", 1, 0 },
-				{ __LINE__, .05555555555,       "60 ms", 1, 0 },
-				{ __LINE__, .55555555555,      "600 ms", 1, 0 },
-				{ __LINE__, 5.5555555555,        "6 s ", 1, 0 },
-				{ __LINE__, 55.555555555,       "60 s ", 1, 0 },
-				{ __LINE__, 555.55555555,      "600 s ", 1, 0 },
-				{ __LINE__, 5555.5555555,        "6 ks", 1, 0 },
+			{ __LINE__, .00555555555,      "5.6 ms", 4, 1 },
+			{ __LINE__, .05555555555,     "55.6 ms", 4, 1 },
+			{ __LINE__, .55555555555,    "555.6 ms", 4, 1 },
+			{ __LINE__, 5.5555555555,      "5.6 s ", 4, 1 },
+			{ __LINE__, 55.555555555,     "55.6 s ", 4, 1 },
+			{ __LINE__, 555.55555555,    "555.6 s ", 4, 1 },
+			{ __LINE__, 5555.5555555,      "5.6 ks", 4, 1 },
 
-				{ __LINE__, .00555555555,        "6 ms", 3, 0 },
-				{ __LINE__, .05555555555,       "56 ms", 3, 0 },
-				{ __LINE__, .55555555555,      "556 ms", 3, 0 },
-				{ __LINE__, 5.5555555555,        "6 s ", 3, 0 },
-				{ __LINE__, 55.555555555,       "56 s ", 3, 0 },
-				{ __LINE__, 555.55555555,      "556 s ", 3, 0 },
-				{ __LINE__, 5555.5555555,        "6 ks", 3, 0 },
+			{ __LINE__, .00555555555,   "5555.6 us", 5, 1 },
+			{ __LINE__, .05555555555,  "55556.0 us", 5, 1 },
+			{ __LINE__, .55555555555, "555560.0 us", 5, 1 },
+			{ __LINE__, 5.5555555555,   "5555.6 ms", 5, 1 },
+			{ __LINE__, 55.555555555,  "55556.0 ms", 5, 1 },
+			{ __LINE__, 555.55555555, "555560.0 ms", 5, 1 },
+			{ __LINE__, 5555.5555555,   "5555.6 s ", 5, 1 },
 
-				{ __LINE__, .00555555555,     "5556 us", 4, 0 },
-				{ __LINE__, .05555555555,    "55560 us", 4, 0 },
-				{ __LINE__, .55555555555,   "555600 us", 4, 0 },
-				{ __LINE__, 5.5555555555,     "5556 ms", 4, 0 },
-				{ __LINE__, 55.555555555,    "55560 ms", 4, 0 },
-				{ __LINE__, 555.55555555,   "555600 ms", 4, 0 },
-				{ __LINE__, 5555.5555555,     "5556 s ", 4, 0 },
+			{ __LINE__, .00555555555,   "5555.56 us", 6, 2 },
+			{ __LINE__, .05555555555,  "55555.60 us", 6, 2 },
+			{ __LINE__, .55555555555, "555556.00 us", 6, 2 },
+			{ __LINE__, 5.5555555555,   "5555.56 ms", 6, 2 },
+			{ __LINE__, 55.555555555,  "55555.60 ms", 6, 2 },
+			{ __LINE__, 555.55555555, "555556.00 ms", 6, 2 },
+			{ __LINE__, 5555.5555555,   "5555.56 s ", 6, 2 },
+		};
 
-				{ __LINE__, .00555555555,      "5.6 ms", 2, 1 },
-				{ __LINE__, .05555555555,     "56.0 ms", 2, 1 },
-				{ __LINE__, .55555555555,    "560.0 ms", 2, 1 },
-				{ __LINE__, 5.5555555555,      "5.6 s ", 2, 1 },
-				{ __LINE__, 55.555555555,     "56.0 s ", 2, 1 },
-				{ __LINE__, 555.55555555,    "560.0 s ", 2, 1 },
-				{ __LINE__, 5555.5555555,      "5.6 ks", 2, 1 },
-
-				{ __LINE__, .00555555555,      "5.6 ms", 4, 1 },
-				{ __LINE__, .05555555555,     "55.6 ms", 4, 1 },
-				{ __LINE__, .55555555555,    "555.6 ms", 4, 1 },
-				{ __LINE__, 5.5555555555,      "5.6 s ", 4, 1 },
-				{ __LINE__, 55.555555555,     "55.6 s ", 4, 1 },
-				{ __LINE__, 555.55555555,    "555.6 s ", 4, 1 },
-				{ __LINE__, 5555.5555555,      "5.6 ks", 4, 1 },
-
-				{ __LINE__, .00555555555,   "5555.6 us", 5, 1 },
-				{ __LINE__, .05555555555,  "55556.0 us", 5, 1 },
-				{ __LINE__, .55555555555, "555560.0 us", 5, 1 },
-				{ __LINE__, 5.5555555555,   "5555.6 ms", 5, 1 },
-				{ __LINE__, 55.555555555,  "55556.0 ms", 5, 1 },
-				{ __LINE__, 555.55555555, "555560.0 ms", 5, 1 },
-				{ __LINE__, 5555.5555555,   "5555.6 s ", 5, 1 },
-
-				{ __LINE__, .00555555555,   "5555.56 us", 6, 2 },
-				{ __LINE__, .05555555555,  "55555.60 us", 6, 2 },
-				{ __LINE__, .55555555555, "555556.00 us", 6, 2 },
-				{ __LINE__, 5.5555555555,   "5555.56 ms", 6, 2 },
-				{ __LINE__, 55.555555555,  "55555.60 ms", 6, 2 },
-				{ __LINE__, 555.55555555, "555556.00 ms", 6, 2 },
-				{ __LINE__, 5555.5555555,   "5555.56 s ", 6, 2 },
-			};
-
-			for (auto &test : tests_set)
-			{	const auto reality = misc::to_string(ch::duration<double>{ test.num_ }, test.significant_, test.decimal_);
-				assert(reality == test.expected_);
-			}
+		for (auto &test : tests_set)
+		{	const auto reality = misc::to_string(ch::duration<double>{ test.num_ }, test.significant_, test.decimal_);
+			assert(reality == test.expected_);
 		}
 
 		return 0;
