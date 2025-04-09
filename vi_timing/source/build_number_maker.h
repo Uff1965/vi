393c1c93ee283b64818528f4a8c7aca68b3ea524
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-04-09 18:19:51 +0300

    build_number_maker is moved to a separate file.

--- vi_timing/source/build_number_maker.h
+++ vi_timing/source/build_number_maker.h
@@ -5,7 +5,8 @@
 namespace misc
 {
 	unsigned build_number_updater(const char(&date)[12], const char(&time)[9]); // Converts the date and time to a monotonically increasing number. Stores it in BUILD_NUMBER.
-	static const auto dummy_build_number_updater = build_number_updater(__DATE__, __TIME__);
+	unsigned build_number_get();
+	static const unsigned dummy_build_number_updater = build_number_updater(__DATE__, __TIME__);
 }
 
 #endif // #ifndef VI_TIMING_SOURCE_BUILD_NUMBER_H
