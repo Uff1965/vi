ea0f39e147a4d531e3b6ee32ae7c9f909bd8f2ab
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-07-08 12:06:21 +0300

    step

--- vi_timing/source/build_number_maker.h
+++ vi_timing/source/build_number_maker.h
@@ -2,15 +2,19 @@
 #	define VI_TIMING_SOURCE_BUILD_NUMBER_MAKER_H
 #	pragma once
 
+#include <cstdint>
+
 namespace misc
 {
+	// Build Number Generator: Formats the build number using the compilation date and time. Example: 2506170933U.
+ 
 	// Updates the global variable based on the last compilation time.
 	unsigned build_number_updater(const char(&date)[12], const char(&time)[9]);
 	// Next line will be called in each translation unit.
-	static const unsigned dummy_build_number_updater = build_number_updater(__DATE__, __TIME__);
+	static const std::uint32_t dummy_build_number_updater = build_number_updater(__DATE__, __TIME__);
 
 	// Returns a number based on the date and time of the last compilation.
-	unsigned build_number_get();
+	std::uint32_t build_number_get();
 }
 
 #endif // #ifndef VI_TIMING_SOURCE_BUILD_NUMBER_MAKER_H
