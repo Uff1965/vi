569d6b77c4b0b123c636068ad39f3d733baf67d2
Ilya Tikhomirov <Uff1965@rambler.ru>
2025-07-31 15:13:37 +0300

    Changed License

--- vi_timing/vi_timing.h
+++ vi_timing/vi_timing.h
@@ -1,27 +1,23 @@
-/********************************************************************\
-'vi_timing' is a compact and lightweight library for measuring code execution
-time in C and C++. 
-
-Copyright (C) 2025 A.Prograamar
-
-This library was created for experimental and educational use. 
-Please keep expectations reasonable. If you find bugs or have suggestions for 
-improvement, contact programmer.amateur@proton.me.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.
-If not, see <https://www.gnu.org/licenses/gpl-3.0.html#license-text>.
-\********************************************************************/
+/*****************************************************************************\
+* This file is part of the 'vi_timing' library.
+* 
+* 'vi_timing' is a compact and lightweight library for measuring code execution
+* time in C and C++. 
+*
+* This library was created for experimental and educational use. Please keep 
+* expectations reasonable. If you find bugs or have suggestions for 
+* improvement, contact <programmer.amateur@proton.me>.
+*
+* No warranties — see LICENSE.txt in project root.
+* Business Source License 1.1 (BSL-1.1):
+*   • Permitted for non-commercial use only.
+*   • Change Date: 2029-09-01 — thereafter under GNU GPLv3.
+* 
+* Attribution notice must be preserved in all copies and derivatives:
+*    “vi_timing Library © A.Prograamar”
+* 
+* For commercial licensing inquiries: <programmer.amateur@proton.me>
+\*****************************************************************************/
 
 #ifndef VI_TIMING_VI_TIMING_H
 #	define VI_TIMING_VI_TIMING_H
